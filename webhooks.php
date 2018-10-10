5cc4cbf3dee75ec074fee9efba6086f2d2a9b729
okkss002 <natthaphoj@hotmail.com>
2018-10-10 16:36:01 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -25,6 +25,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			$messages = 'ทดสอบ';
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
